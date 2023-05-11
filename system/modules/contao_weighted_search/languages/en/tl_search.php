d25e576322ddc3ee0ff268a7f0bb7b3954f761b3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-05-11 14:50:02 -0400

    Create tl_search.php

---
+++ system/modules/contao_weighted_search/languages/en/tl_search.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Weighted Search
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-weighted-search
+ * @link       https://www.brightcloudstudio.com/
+ * @license    https://opensource.org/license/gpl-3-0/
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_search']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_search']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_search']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_search']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_search']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_search']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* Approved Details */
+$GLOBALS['TL_LANG']['tl_search']['search_index_legend']        = 'Search Index Values';
+$GLOBALS['TL_LANG']['tl_search']['url']                        = array('URL', 'The URL of this entry');
+$GLOBALS['TL_LANG']['tl_search']['weight']                     = array('Weight', 'The custom assigned Weight. Higher numbers display first.');
