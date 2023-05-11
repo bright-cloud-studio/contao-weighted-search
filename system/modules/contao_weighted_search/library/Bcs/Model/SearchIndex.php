e4349dc8fc08478fbea0d57f1471217abf7ff49a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-05-11 14:48:26 -0400

    Create SearchIndex.php

---
+++ system/modules/contao_weighted_search/library/Bcs/Model/SearchIndex.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Weighted Search
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-weighted-search
+ * @link       https://www.brightcloudstudio.com/
+ * @license    https://opensource.org/license/gpl-3-0/
+**/
+
+namespace Bcs\Model;
+
+class SearchIndex extends \Model
+{
+	/* Table name */
+	protected static $strTable = 'tl_search';	
+}
