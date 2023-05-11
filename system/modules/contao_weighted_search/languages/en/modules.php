30638f145270d25822f2c5453a3ec7f5db272861
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-05-11 14:49:40 -0400

    Create modules.php

---
+++ system/modules/contao_weighted_search/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Weighted Search
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-weighted-search
+ * @link       https://www.brightcloudstudio.com/
+ * @license    https://opensource.org/license/gpl-3-0/
+**/
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['search_index']          = array('Search Index', 'Displays entries in the auto-generated Search Index');
